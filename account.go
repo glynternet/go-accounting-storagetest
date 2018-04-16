24c5a40ab146909aa07c019497859569233fb130
Glyn Owen Hanmer <glynhanmer@gmail.com>
2018-04-16 20:30:50 +0200

    Return pointer to Account to reflect new go-accounting API

--- account.go
+++ account.go
@@ -9,7 +9,7 @@ import (
 	"github.com/glynternet/go-money/currency"
 )
 
-func NewAccount(t *testing.T, name string, c currency.Code, open time.Time, os ...account.Option) account.Account {
+func NewAccount(t *testing.T, name string, c currency.Code, open time.Time, os ...account.Option) *account.Account {
 	a, err := account.New(name, c, open, os...)
 	common.FatalIfError(t, err, "Creating new Account")
 	return a
